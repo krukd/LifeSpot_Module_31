749ebabe4f2f0b850e1d072c36ca1817740f2e8c
krukd <krukd87@gmail.com>
2024-01-10 19:42:09 +0100

    HW_Module_31

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,42 @@
+﻿function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+// Логирование сессии (объявлено через expression)
+let sessionLog = function logSession() {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+/*
+* Обработчик клика
+*
+* */
+const saveInput = function () {
+    // Вытащим значение текстового поля
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
